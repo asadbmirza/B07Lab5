0a51fe53e4cfa6634cb6c940133330e306d46bce
Jeremy Chong <chong.jeremy@hotmail.com>
2024-10-23 23:06:05 -0400

    chore: add tests for Circle class; add tests for constructor, area(), and equals() method

---
+++ src/CircleTests1.java
@@ -0,0 +1,57 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import Circle;
+
+class CircleTests1 {
+
+	@Test
+	void testConstructorRadius() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertEquals(circle.radius, 1);
+	}
+	
+	@Test
+	void testConstructorPoint() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertEquals(circle.center, new Point(0, 0));
+	}
+	
+	@Test
+	void testArea() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertEquals(circle.area(), Math.PI);
+	}
+	
+	@Test
+	void testEqualsReflexitivity() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertTrue(circle.equals(), circle);
+	}
+	
+
+	@Test
+	void testEqualsNull() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertFalse(circle.equals(null));
+	}
+	
+	@Test
+	void testEqualsClass() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertFalse(circle.equals(new Point(1, 1)));
+	}
+	
+	@Test
+	void testEqualsFail() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertFalse(circle.equals(new Circle(1, new Point(1, 1))));
+	}
+	
+	@Test
+	void testEqualsPass() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertTrue(circle.equals(new Circle(1, new Point(0, 0))));
+	}
+	
+}
