25c78156e3750d9293b8eb44400fa4fe37cd5a2f
gbg7 <57051706+GBG7@users.noreply.github.com>
2024-10-27 15:05:53 -0400

    updated circletests1
    added test cases for circle.circumference() & circle.hashCode()

---
+++ src/CircleTests1.java
@@ -0,0 +1,99 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import Circle;
+
+class CircleTests1 {
+
+	@Test
+	void testConstructorRadius() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertEquals(circle.radius, 1);
+	}
+	
+	@Test
+	void testConstructorPoint() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertEquals(circle.center, new Point(0, 0));
+	}
+	
+	@Test
+	void testArea() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertEquals(circle.area(), Math.PI);
+	}
+	
+	@Test
+	void testEqualsReflexitivity() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertTrue(circle.equals(circle));
+	}
+	
+
+	@Test
+	void testEqualsNull() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertFalse(circle.equals(null));
+	}
+	
+	@Test
+	void testEqualsClass() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertFalse(circle.equals(new Point(1, 1)));
+	}
+	
+	@Test
+	void testEqualsFail() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertFalse(circle.equals(new Circle(1, new Point(1, 1))));
+	}
+	
+	@Test
+	void testEqualsPass() {
+		Circle circle = new Circle(1, new Point(0, 0));
+		assertTrue(circle.equals(new Circle(1, new Point(0, 0))));
+	}
+
+
+	// ----
+	@Test
+	void testBasicCircleCircumference(){
+		Point p1 = new Point(1,2);
+		Circle c = new Circle(3, p1);
+		assertEquals(c.circumference(),2*Math.PI * c.radius);
+	}
+
+	@Test
+	void testCircumferenceZeroRadius() {
+        Circle circle = new Circle(0.0, new Point(0.0, 0.0));
+        double exp = 0.0;
+        assertEquals(exp, circle.circumference());
+    }
+
+	@Test
+	void testHashCodeEqualityWithEqualCircles() {
+        Point center1 = new Point(3.0, 4.0);
+        Circle circle1 = new Circle(7.0, center1);
+        Circle circle2 = new Circle(7.0, new Point(3.0, 4.0));
+        assertEquals(circle1.hashCode(), circle2.hashCode());
+    }
+	
+	@Test
+	void testHashCodeEquals() {
+		Point p3 = new Point(1,2);
+		Circle c = new Circle(2, p3);
+		Point p4 = new Point(1,2);
+		Circle c1 = new Circle(2, p4);
+		assertTrue(c.hashCode() == c1.hashCode());
+	}
+	
+	@Test
+	void testHashCodeNotEquals() {
+		Point p3 = new Point(1,2);
+		Circle c = new Circle(2, p3);
+		Point p4 = new Point(2,2);
+		Circle c1 = new Circle(3, p4);
+		assertFalse(c.hashCode() == c1.hashCode());
+	}
+	
+}
---
+++ src/Lab4Tests.java
@@ -0,0 +1,117 @@
+import lab4.Point;
+import lab4.Triangle;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Lab4Tests {
+
+	// Test for Point class
+
+    @Test
+    void testDistanceSamePoint() {
+        Point p1 = new Point(0, 0);
+        assertEquals(0, p1.distance(p1));
+    }
+
+    @Test
+    void testDistanceDifferentPoints() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3, 4);
+        assertEquals(5, p1.distance(p2));
+    }
+
+    @Test
+    void testEqualsSameObject() {
+        Point p1 = new Point(1, 1);
+        assertTrue(p1.equals(p1));
+    }
+
+    @Test
+    void testEqualsDifferentXPoints() {
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(2, 2);
+        assertFalse(p1.equals(p2));
+    }
+    
+    @Test
+    void testEqualsDifferentYPoints() {
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(1, 2);
+        assertFalse(p1.equals(p2));
+    }
+    
+    @Test
+    void testEqualsDifferentClass() {
+        Point p1 = new Point(1, 1);
+        String notAPoint = "Not a Point";
+        assertFalse(p1.equals(notAPoint));
+    }
+
+    @Test
+    void testEqualsNull() {
+        Point p1 = new Point(1, 1);
+        assertFalse(p1.equals(null));
+    }
+
+    @Test
+    void testEqualsSameCoordinates() {
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(1, 1);
+        assertTrue(p1.equals(p2));
+    }
+
+    @Test
+    void testHashCode() {
+        Point p1 = new Point(1, 1);
+        assertEquals(8, p1.hashCode()); // 3*1 + 5*1 = 8
+    }
+
+    // Test for Triangle class
+
+    @Test
+    void testPerimeter() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3, 0);
+        Point p3 = new Point(0, 4);
+        Triangle t = new Triangle(p1, p2, p3);
+        assertEquals(12, t.perimeter());
+    }
+
+    @Test
+    void testIsEquilateralTrue() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(Math.sqrt(5), 0);
+        Point p3 = new Point(Math.sqrt(5)/2, Math.sqrt(15)/2);
+        Triangle t = new Triangle(p1, p2, p3);
+        assertTrue(t.isEquilateral());
+    }
+
+    @Test
+    void testIsEquilateralFalse() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3, 0);
+        Point p3 = new Point(0, 4);
+        Triangle t = new Triangle(p1, p2, p3);
+        assertFalse(t.isEquilateral());
+    }
+
+    @Test
+    void testIsEquilateralFirstFalse() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(4, 0);
+        Point p3 = new Point(2, Math.sqrt(12));
+        Triangle t = new Triangle(p1, p2, p3);
+        assertFalse(t.isEquilateral());
+    }
+
+    @Test
+    void testIsEquilateralSecondFalse() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(2, 0);
+        Point p3 = new Point(2, 2);
+        Triangle t = new Triangle(p1, p2, p3);
+        assertFalse(t.isEquilateral());
+    }
+
+}
